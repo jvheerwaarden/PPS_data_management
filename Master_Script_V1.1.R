ffa396161ccd4c6e5162b1f7b0df6effa97400ce
Joost van Heerwaarden <jvheerwaarden@users.noreply.github.com>
2020-06-16 18:36:56 +0200

    Update Master_Script_V1.1.R

--- Master_Script_V1.1.R
+++ Master_Script_V1.1.R
@@ -31,7 +31,7 @@ setwd(wd)
 file.list.wd.initial= list.files(wd)
 
 if(sum(grepl("example_data", file.list.wd.initial))==0) download.file("https://raw.githubusercontent.com/jvheerwaarden/PPS_data_management/master/example_data.csv", destfile="example_data.csv")
-if(sum(grepl("Documentation", file.list.wd.initial))==0) download.file("https://github.com/jvheerwaarden/PPS_data_management/blob/master/Documentation.docx","Documentation.docx")
+if(sum(grepl("Documentation", file.list.wd.initial))==0) download.file("https://github.com/jvheerwaarden/PPS_data_management/blob/master/Documentation.docx?raw=true","Documentation.docx")
 if(sum(grepl("Readme", file.list.wd.initial))==0) download.file("https://raw.githubusercontent.com/jvheerwaarden/PPS_data_management/master/Readme","Readme.txt")
 
 if(sum(grepl("0_build_project", file.list.wd.initial))==0) download.file("https://raw.githubusercontent.com/jvheerwaarden/PPS_data_management/master/0_build_project.R", destfile="0_build_project.R")
