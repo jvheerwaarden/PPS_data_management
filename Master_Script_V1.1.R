7b18d776c9f8031c529ac8b3fc4b2581287ca147
Joost van Heerwaarden <jvheerwaarden@users.noreply.github.com>
2020-06-18 14:47:56 +0200

    Update Master_Script_V1.1.R

--- Master_Script_V1.1.R
+++ Master_Script_V1.1.R
@@ -37,6 +37,7 @@ if(sum(grepl("0_build_project", file.list.wd.initial))==0) download.file("https:
 if(sum(grepl("1_data_cleaning", file.list.wd.initial))==0) download.file("https://raw.githubusercontent.com/jvheerwaarden/PPS_data_management/master/1_data_cleaning.R", destfile="1_data_cleaning.R")
 if(sum(grepl("2_analysis.R", file.list.wd.initial))==0) download.file("https://raw.githubusercontent.com/jvheerwaarden/PPS_data_management/master/2_analysis.R", destfile="2_analysis.R")
 if(sum(grepl("MiRAE_funcs", file.list.wd.initial))==0) download.file("https://raw.githubusercontent.com/jvheerwaarden/PPS_data_management/master/MiRAE_funcs.R", destfile="MiRAE_funcs.R")
+if(sum(grepl("RawDataConvertScript", file.list.wd.initial))==0) download.file("https://raw.githubusercontent.com/jvheerwaarden/PPS_data_management/master/RawDataConvertScript.R", destfile="RawDataConvertScript.R")
 if(sum(grepl("Meststof proef WUR.csv", file.list.wd.initial))==0) download.file("https://raw.githubusercontent.com/jvheerwaarden/PPS_data_management/master/Meststof%20proef%20WUR.csv", destfile="Meststof proef WUR.csv")
 #if(sum(grepl("Meststof proef WUR_metadata.xlsx", file.list.wd.initial))==0 download.file("https://github.com/ALanguillaume/MiRAE/blob/master/data/raw/Meststof%20proef%20WUR_metadata.xlsx?raw=true", destfile="Meststof proef WUR_metadata.xlsx")
 
